e1d2d08190ae02c309bcd03a3c47943c5f3700bc
Keerthana215 <50616071+Keerthana215@users.noreply.github.com>
2019-07-12 05:23:22 -0700

    Update vowel.py

--- vowel.py
+++ vowel.py
@@ -1,8 +1,8 @@
 wo = input()
 if((wo>='a' and wo<= 'z') or (wo>='A' and wo<='Z')):
     if(wo=='A' or wo=='a' or wo=='E' or wo =='e' or wo=='I' or wo=='i' or wo=='O' or wo=='o' or wo=='U' or wo=='u'):
-        print(wo, "is a Vowel")
+        print("Vowel")
     else:
-        print(wo, "is a Consonant")
+        print("Consonant")
 else:
     print("Invalid")
