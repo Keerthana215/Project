0542f9dffba3ce3f4311c1d7a613a3b49a69ea57
Keerthana215 <50616071+Keerthana215@users.noreply.github.com>
2019-07-12 05:11:37 -0700

    Update even.py

--- even.py
+++ even.py
@@ -1,8 +1,8 @@
 num=float(input())
 if num>0:
-if(num%2==0):
-print("Even")
+  if(num%2==0):
+    print("Even")
+  else:
+    print("Odd")
 else:
-print("Odd")
-else:
-print("Invalid")
+  print("Invalid")
